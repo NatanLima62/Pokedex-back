b3bfa628ea795e8ff35443f4edad8d85438a5846
Natanael Lima <natanlimaoliveira62@gmail.com>
2023-10-23 10:44:28 -0300

    addiniciais

---
+++ Pokedex.Infra/Migrations/20231023132544_AddPokemonsIniciais.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pokedex.Infra.Migrations
+{
+    public partial class AddPokemonsIniciais : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Pokemons",
+                columns: new[] { "Id", "Nome", "Descricao", "PokemonTipoId", "Imagem"},
+                values: new object[,]
+                {
+                    { 1, "Bulbasaur", "Pokemón inicial da primeira geração", 4, "../../assets/bulba.png"},
+                    { 2, "Squirtle", "Pokemón inicial da primeira geração", 3, "../../assets/squirtle.png"},
+                    { 3, "Charmander", "Pokemón inicial da primeira geração", 2, "../../assets/charmander.png"},
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder
+                .DeleteData("Pokemons", "Id", 1);
+        }
+    }
+}
